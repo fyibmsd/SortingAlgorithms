f083a35ba402c42812b08d41a3e48e82ebf8fdb4
fyibmsd <fyibmsd@gmail.com>
2017-02-10 17:20:32 +0800

    update

--- src/BubbleSort.js
+++ src/BubbleSort.js
@@ -8,7 +8,7 @@ module.exports = function BubbleSort(arr) {
     let len = arr.length;
 
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
+        for (let j = 0; j < len - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
--- test/test_sort.js
+++ test/test_sort.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const Sort     = require('../src');
-const unsorted = [31, 41, 59, 26, 53, 58, 97, 93, 23, 84];
-const sorted   = [23, 26, 31, 41, 53, 58, 59, 84, 93, 97];
-const tester   = ((x, y) => (f) => require('assert').deepEqual(f(x), y))(unsorted, sorted);
+const unsorted = () => [31, 41, 59, 26, 53, 58, 97, 93, 23, 84];
+const sorted   = () => [23, 26, 31, 41, 53, 58, 59, 84, 93, 97];
+const tester   = ((x) => (f) => require('assert').deepEqual(f(unsorted()), x))(sorted());
 
 describe("Test sort algorithms", () => {
     it("Test bubble sort", () => tester(Sort.bubbleSort));
